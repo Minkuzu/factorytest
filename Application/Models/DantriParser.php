611be5348d2f63e44e2dbf4f47ffd9f2c4564a73
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-07 09:40:10 +0700

    change getTitle back to crawl data from url

--- Application/Models/DantriParser.php
+++ Application/Models/DantriParser.php
@@ -15,7 +15,23 @@ class DantriParser extends Parser
     }
     public function getTitle($url)
     {
-        echo "getTitle";
+        global $class;
+        $url = "https://dantri.com.vn/xa-hoi/quoc-hoi-hop-rieng-ve-cong-tac-nhan-su-20240605220331292.htm";
+        $html = $this->GetUrl($url);
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+
+        $xpath = new DomXPath($dom);
+        $class = 'title-page detail';
+        $divs = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]");
+
+        foreach($divs as $div) 
+        {
+            echo $div->nodeValue;
+            // If you want to include the html elements too:
+            // echo $dom->saveXML($div);   
+        }
     }
     public function getArticle($url)
     {
@@ -38,7 +54,7 @@ class DantriParser extends Parser
     }
     public function getDate($url)
     {
-        echo "getDate";
+        echo $url;
     }
 }
 ?>
