261c3dc068f43911f245d22ece10b944274f0d39
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-13 10:28:18 +0700

    update vnexpress parser

--- Application/Models/VnexpressParser.php
+++ Application/Models/VnexpressParser.php
@@ -3,17 +3,40 @@ namespace App\Models;
 use App\Models\Parser;
 use DOMDocument;
 use DOMXPath;
-class VnexpressParser
+class VnexpressParser extends Parser
 {
-    public function test()
+    public function crawlProcess($class)
     {
-        echo "Hello Dantri";
+        $url = "https://dantri.com.vn/the-thao/bao-philippines-binh-luan-khi-doi-nha-thua-dau-doi-tuyen-viet-nam-20240607003627722.htm";
+        $html = $this->GetUrl($url);
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+
+        $xpath = new DomXPath($dom);
+        $divs = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]");
+
+        foreach($divs as $div) 
+        {
+            $data = $div->nodeValue;
+            // If you want to include the html elements too:
+            // echo $dom->saveXML($div);   
+        }
+        return $data;
+    }
+    public function getParser()
+    {
+
     }
     public function getTitle($url)
     {
 
     }
     public function getDate($url)
+    {
+
+    }
+    public function getArticle($url)
     {
         
     }
