73094a949d178c669075c6b64237272685d2f6ee
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-13 10:24:47 +0700

    move crawlProcess function to base class

--- Application/Models/Parser.php
+++ Application/Models/Parser.php
@@ -1,6 +1,27 @@
 <?php
 namespace App\Models;
+use DOMDocument;
+use DOMXPath;
 abstract class Parser{
+    public function crawlProcess($class)
+    {
+        $url = "https://dantri.com.vn/the-thao/bao-philippines-binh-luan-khi-doi-nha-thua-dau-doi-tuyen-viet-nam-20240607003627722.htm";
+        $html = $this->GetUrl($url);
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+
+        $xpath = new DomXPath($dom);
+        $divs = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]");
+
+        foreach($divs as $div) 
+        {
+            $data = $div->nodeValue;
+            // If you want to include the html elements too:
+            // echo $dom->saveXML($div);   
+        }
+        return $data;
+    }
     public function getUrl($url)
     {
         $ch = curl_init();
