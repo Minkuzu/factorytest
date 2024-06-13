f7470f50127a447478620f42e9421e550b909df9
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-13 10:24:28 +0700

    move crawProcess function to base class

--- Application/Models/DantriParser.php
+++ Application/Models/DantriParser.php
@@ -5,25 +5,25 @@ use DOMDocument;
 use DOMXPath;
 class DantriParser extends Parser
 {
-    public function crawlProcess($class)
-    {
-        $url = "https://dantri.com.vn/the-thao/bao-philippines-binh-luan-khi-doi-nha-thua-dau-doi-tuyen-viet-nam-20240607003627722.htm";
-        $html = $this->GetUrl($url);
+    // public function crawlProcess($class)
+    // {
+    //     $url = "https://dantri.com.vn/the-thao/bao-philippines-binh-luan-khi-doi-nha-thua-dau-doi-tuyen-viet-nam-20240607003627722.htm";
+    //     $html = $this->GetUrl($url);
 
-        $dom = new DOMDocument();
-        @$dom->loadHTML($html);
+    //     $dom = new DOMDocument();
+    //     @$dom->loadHTML($html);
 
-        $xpath = new DomXPath($dom);
-        $divs = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]");
+    //     $xpath = new DomXPath($dom);
+    //     $divs = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]");
 
-        foreach($divs as $div) 
-        {
-            $data = $div->nodeValue;
-            // If you want to include the html elements too:
-            // echo $dom->saveXML($div);   
-        }
-        return $data;
-    }
+    //     foreach($divs as $div) 
+    //     {
+    //         $data = $div->nodeValue;
+    //         // If you want to include the html elements too:
+    //         // echo $dom->saveXML($div);   
+    //     }
+    //     return $data;
+    // }
     public function getParser()
     {
         echo "getParser";
