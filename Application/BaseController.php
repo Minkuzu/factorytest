30a445452fb8d229771e3bc91a47b8aeef6d19b8
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-14 10:17:44 +0700

    minor changes

--- Application/BaseController.php
+++ Application/BaseController.php
@@ -3,6 +3,7 @@ namespace App;
 class BaseController
 {
     public $folder;
+    
     public function render($file, $data = [])
     {
         $viewFile = "Views/" . $this->folder . "/" . $file . ".php"; //  Views/whicheverControllerCalls/viewfile.php
