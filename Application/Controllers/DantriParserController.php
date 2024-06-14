d86c786ef93aa5af77f9b381856276bf27e0c67c
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-14 11:38:01 +0700

    store object function into variable

--- Application/Controllers/DantriParserController.php
+++ Application/Controllers/DantriParserController.php
@@ -3,6 +3,7 @@ namespace App\Controllers;
 use App\BaseController; // composer doesn't work
 use App\Models\DantriParser;
 require __DIR__ . "/../BaseController.php";
+
 class DantriParserController extends BaseController
 {
     public $dantriParser; 
@@ -14,11 +15,22 @@ class DantriParserController extends BaseController
     public function home()
     {
         $url = $_POST['input'];
+        $title = $this->dantriParser->getTitle($url);
+        $date = $this->dantriParser->getDate($url);
+        $article = $this->dantriParser->getArticle($url);
         $data = [
-            'title' => $this->dantriParser->getTitle($url),
-            'date' => $this->dantriParser->getDate($url),
-            'article' => $this->dantriParser->getArticle($url)
+            'title' => $title,
+            'date' => $date,
+            'article' => $article
             ];
+        require __DIR__ . "/../../connection.php";
+        $sql = "INSERT INTO DanTri (danTriUrl, title, content, date_created)
+        VALUES ('$url', '$title', '$article', '$date')";
+        if ($conn->query($sql) === TRUE) {
+          echo "Data successfully inserted into table";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
         $this->render('home', $data);
     }
 }
