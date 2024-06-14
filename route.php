190bacc95213774f11d6b3c48b0a237a244185d5
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-14 10:18:22 +0700

    add distinguish
    to identify which newspaper is being input in

--- route.php
+++ route.php
@@ -3,36 +3,46 @@
 use App\Controllers\DantriParserController;
 use App\Controllers\VnexpressParserController;
 function call($controller, $action) {
-    require_once "Application/Controllers/" . $controller . "Controller.php";
+  require_once "Application/Controllers/" . $controller . "Controller.php";
 
-    switch($controller) { // using switch doesn't scale well
-      case "Pages":
-        $controller = new PagesController();
-      break;
-      case "DantriParser":
-        // we need the model to query the database later in the controller
-        require_once "Application/Models/DantriParser.php";
-        $controller = new DantriParserController();
-      break;
-      case "VnexpressParser":
-        require_once "Application/Models/VnexpressParser.php";
-        $controller = new VnexpressParserController();
-      break;
-    }
-    $controller->$action();
+  switch($controller) { // using switch doesn't scale well
+    case "Pages":
+      $controller = new PagesController();
+    break;
+    case "DantriParser":
+      // we need the model to query the database later in the controller
+      require_once "Application/Models/DantriParser.php";
+      $controller = new DantriParserController();
+    break;
+    case "VnexpressParser":
+      require_once "Application/Models/VnexpressParser.php";
+      $controller = new VnexpressParserController();
+    break;
+  }
+  $controller->$action();
 }
 
-
+$url = $_POST['input'];
 // we're adding an entry for the new controller and its actions
 $controllers = array(   'Pages' => ['home', 'error'],
                         'DantriParser' => ['home'],
                         'VnexpressParser' => ['home']
                     );
-if (array_key_exists($controller, $controllers)) {
+if(isset($url)){
+  if(str_contains($url, "dantri.com.vn")){
+    $controller = 'DantriParser';
+    $action = 'home';
+    call($controller, $action);
+  } else if(str_contains($url , "vnexpress.net")){
+    $controller = 'VnexpressParser';
+    $action = 'home';
+    call($controller, $action);
+  }
+} else {
+  if (array_key_exists($controller, $controllers)) {
     if (in_array($action, $controllers[$controller])) {
         call($controller, $action);
         echo "Call to function successfully";
-        echo $_POST['input'];
     } else {
         call('pages', 'error');
         echo "something wrong";
@@ -41,4 +51,18 @@ if (array_key_exists($controller, $controllers)) {
     call('pages', 'error');
     echo "something wrong";
 }
+}
+// if (array_key_exists($controller, $controllers)) {
+//     if (in_array($action, $controllers[$controller])) {
+//         call($controller, $action);
+//         distinguish($url);
+//         echo "Call to function successfully";
+//     } else {
+//         call('pages', 'error');
+//         echo "something wrong";
+//     }
+// } else {
+//     call('pages', 'error');
+//     echo "something wrong";
+// }
 ?>
