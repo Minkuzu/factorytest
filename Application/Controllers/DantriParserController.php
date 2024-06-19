601a6df9ebd9524233023fbeb8a1111a0289dd31
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-19 09:06:07 +0700

    Update DantriParserController.php

--- Application/Controllers/DantriParserController.php
+++ Application/Controllers/DantriParserController.php
@@ -24,36 +24,12 @@ class DantriParserController extends BaseController
     }
     public function home()
     {
-        // $url = $_POST['input'];
-        // $this->dantriParser->getClass($url);
-        // $title = $this->dantriParser->getTitle($url);
-        // $date = $this->dantriParser->getDate($url);
-        // $article = $this->dantriParser->getArticle($url);
         $elements = $this->getVariables();
         $data = [
             'title' => $elements[1],
             'article' => $elements[2],
             'date' => $elements[3]
             ];              
-        // require __DIR__ . "/../../connection.php";
-        // $sql = "SELECT danTriUrl FROM DanTri WHERE danTriUrl LIKE '$url'";
-        // //  Get results from query
-        // $result = mysqli_query($conn, $sql);
-        // //  Check if there are any record match with the url
-        // if(mysqli_num_rows($result) == 0)
-        // {
-        //     $sql2 = "INSERT INTO DanTri (danTriUrl, title, content, date_created)
-        //     VALUES ('$url', '$title', '$article', '$date')";
-        //     if ($conn->query($sql2) === TRUE) {
-        //         echo "Data successfully inserted into table!";
-        //     } else {
-        //         echo "Error: " . $sql . "<br>" . $conn->error;
-        //     }
-        // //  If yes, don't insert
-        // } elseif (mysqli_num_rows($result) == 1)
-        // {
-        //     echo "This url is already in database!";
-        // }
         $this->saveToDB($elements);
         $this->render('home', $data);
     }
