627128a34f572694ab91c5fa29334ea53b133a3a
Mink <minhndgdh190046@fpt.edu.vn>
2024-06-25 12:07:41 +0700

    add ParserFactory.php

---
+++ Application/ParserFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\DantriParser;
+use App\Models\Parser;
+use App\Models\VnexpressParser;
+
+require __DIR__ . "/Models/Parser.php";
+
+class ParserFactory {
+    public function getParser($parserType){
+        switch ($parserType) {
+            case "dantri":
+                return new DantriParser;
+            case "vnexpress":
+                return new VnexpressParser;
+            default:
+                break;
+        }
+    }
+}
+?>
