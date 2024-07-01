f28291d4bae5c3239a7970d7add7cec2c556f870
Mink <minhndgdh190046@fpt.edu.vn>
2024-07-01 11:14:14 +0700

    fixed categories validation for vnexpress

--- route.php
+++ route.php
@@ -10,37 +10,40 @@ function call($controller, $action) {
       $controller = new PagesController();
       break;
       case "DantriParser":
-        // we need the model to query the database later in the controller
+        // need the model to query the database later in the controller
         $controller = new DantriParserController();
         break;
       case "VnexpressParser":
         $controller = new VnexpressParserController();
         break;
   }
+  //  Call the function of destinated controller
   $controller->$action();
 }
 
 $url = $_POST['input'];
 
 $path = parse_url($url, PHP_URL_PATH);
-// we're adding an entry for the new controller and its actions
+// adding an entry for the new controller and its actions
 $controllers = array(   'Pages' => ['home', 'error'],
                         'DantriParser' => ['home'],
                         'VnexpressParser' => ['home']
                     );
 if (empty($url) == false)  {
   require_once "Curl.php";
-  if (str_contains($url, "dantri.com.vn") && array_search($path, $fixedCategories) == false) { // false means newspaper
+//  If url contains dantri.com.vn and path not included
+  if (str_contains($url, "dantri.com.vn") && array_search($path, $DanTriCategories) == false) { // false means newspaper
     $controller = 'DantriParser';
     $action = 'home';
     call($controller, $action);
-  } else if (str_contains($url , "vnexpress.net") && array_search($path, $categories) == false) {
+//  If url contains vnexpress.net and path not included
+  } else if (str_contains($url , "vnexpress.net") && array_search($path, $allCategories) == false) {
     $controller = 'VnexpressParser';
     $action = 'home';
     call($controller, $action);
+//  Call to error page
   } else {
     call('Pages','error');
-    var_dump(array_search($path, $allCategories));
     echo "not redirected";
   }
 } elseif (array_key_exists($controller, $controllers)) {
