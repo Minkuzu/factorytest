2c869a9997c4d0d48d868a457b193f710e7a6328
Mink <minhndgdh190046@fpt.edu.vn>
2024-07-01 11:12:49 +0700

    remove saveToDB function
    replace with $this->parser->addNews

--- Application/Controllers/VnexpressParserController.php
+++ Application/Controllers/VnexpressParserController.php
@@ -3,6 +3,7 @@ namespace App\Controllers;
 use App\BaseController; // composer doesn't work
 use App\Models\VnexpressParser;
 
+require __DIR__ . "/../Models/VnexpressParser.php";
 require __DIR__ . "/../BaseController.php";
 
 class VnexpressParserController extends BaseController {
@@ -27,28 +28,8 @@ class VnexpressParserController extends BaseController {
             'article' => $elements[2],
             'date' => $elements[3]
             ];
-        $this->saveToDB($elements);
+        $this->parser->addNews($elements[0], $elements[1], $elements[2], $elements[3]);
         $this->render('home', $data);
-    }
-
-    public function saveToDB($elements) {
-        require __DIR__ . "/../../connection.php";
-        $sql = "SELECT vnExpressUrl FROM VnExpress WHERE vnExpressUrl LIKE '$elements[0]'";
-        //  Get results from query
-        $result = mysqli_query($conn, $sql);
-        //  Check if there are any record match with the url
-        if (mysqli_num_rows($result) == 0) {
-            $sql2 = "INSERT INTO VnExpress (vnExpressUrl, title, content, date_created)
-            VALUES ('$elements[0]', '$elements[1]', '$elements[2]', '$elements[3]')";
-            if ($conn->query($sql2) === TRUE) {
-                echo "Data successfully inserted into table!";
-            } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        //  If yes, don't insert
-        } elseif (mysqli_num_rows($result) == 1) {
-            echo "This url is already in database!";
-        }
     }
 }
 ?>
