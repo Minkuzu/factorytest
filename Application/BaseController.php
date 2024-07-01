23d3877da84b72c9f257d59f32b2ac2155958aa1
Mink <minhndgdh190046@fpt.edu.vn>
2024-07-01 12:06:06 +0700

    refactor function

--- Application/BaseController.php
+++ Application/BaseController.php
@@ -3,6 +3,36 @@ namespace App;
 class BaseController
 {
     protected $folder;
+    protected $elements;
+    public function __construct()
+    {
+        $this->elements = $this->getParserAttribute();
+    }
+    //  might not necessary
+    public function getParserAttribute($parser) {
+        $url = $_POST['input'];
+        $parser->getClass($url);
+        $title = $parser->getTitle($url);
+        $date = $parser->getDate($url);
+        $article = $parser->getArticle($url);
+        return array($url, $title, $article, $date);
+    }
+    //  change function name to verb first -> noun
+    public function extractData($parser) {
+        $url = $_POST['input'];
+        $parser->getClass($url);
+        $title = $parser->getTitle($url);
+        $date = $parser->getDate($url);
+        $article = $parser->getArticle($url);
+        return array($url, $title, $article, $date);
+        $data = [
+            'title' => $this->elements[1],
+            //'title' => $title = $parser->getTitle($url),
+            'article' => $this->elements[2],
+            'date' => $this->elements[3]
+            ];   
+        return $data;         
+    }
     public function render($file, $data = []) {
         $viewFile = "Views/" . $this->folder . "/" . $file . ".php"; //  Views/whicheverControllerCalls/viewfile.php
         extract($data);  
